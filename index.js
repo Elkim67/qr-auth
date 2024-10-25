41344de92c610a74ee9c49161931cd7e1e153a3f
Elkim67 <eliekiwoy67@gmail.com>
2024-10-25 04:15:54 +0100

    second commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(
 );
 
 app.get("/accueil", (req, res) => {
-  res.sendFile(path.join(initial_path, "accueil.html"));
+  res.sendFile(path.join(initial_path, "index.html"));
 });
 
 // Route pour le formulaire de connexion
@@ -98,7 +98,7 @@ app.post("/login", (req, res) => {
       if (results.length > 0) {
         // Authentification réussie
         req.session.userId = results[0].id;
-        res.redirect("/accueil"); // Redirection vers la page souhaitée
+        res.redirect("/index"); // Redirection vers la page souhaitée
       } else {
         // Authentification échouée
         res.sendFile(path.join(initial_path, "error.html"));
